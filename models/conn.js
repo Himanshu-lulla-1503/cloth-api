e69555f2c2704f6a0c495344601f052a7652a94e
Himanshu lulla <chinululla87@gmail.com>
2021-05-27 23:05:12 +0530

    Solved buffered timeout query

--- models/conn.js
+++ models/conn.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-module.exports=mongoose.connect(process.env.mydb,{useNewUrlParser: true,useUnifiedTopology: true}).then(()=>{
+module.exports=mongoose.connect(process.env.mydb1,{useNewUrlParser: true,useUnifiedTopology: true}).then(()=>{
     console.log("connected successfully ");
 }).catch((err)=>{
     console.log(err);
