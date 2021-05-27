cebf3ebb0e3d1cf3650463fae898e2e46ef286e8
Himanshu lulla <chinululla87@gmail.com>
2021-05-27 23:35:33 +0530

    revised

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ require('dotenv').config();
 const connection = require('./models/conn');
 const Userroute=require('./routes/UserRouter');
 const Clothesroute=require('./routes/ClothRouter');
+//Allow all requests from all domains & localhost
+app.all('/*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "POST, GET");
+    next();
+  });
 //Routing of /users endpoint to UserRouter.js file
 app.use('/users',Userroute);
 //Routing of /clothes endpoint to ClothesRouter.js file
