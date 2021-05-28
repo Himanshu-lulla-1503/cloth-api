05ed526632ffa194225a400541059e07c0c916af
Himanshu lulla <chinululla87@gmail.com>
2021-05-28 18:26:04 +0530

    Works

--- models/conn.js
+++ models/conn.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-module.exports=mongoose.connect(process.env.mydb1,{useNewUrlParser: true,useUnifiedTopology: true}).then(()=>{
+module.exports=mongoose.connect(process.env.mydb1,{useNewUrlParser: true,useUnifiedTopology: true,useFindAndModify:false,useCreateIndex:true}).then(()=>{
     console.log("connected successfully ");
 }).catch((err)=>{
     console.log(err);
